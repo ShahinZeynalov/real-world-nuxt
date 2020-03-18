3e5e5fd5e1af5056a891aaf24d1c683359173283
Shahin Zeynalov <shahin.zeynalov@bk.ru>
2020-03-18 19:08:43 +0400

    Some works done

--- pages/create.vue
+++ pages/create.vue
@@ -1,15 +1,13 @@
 <template lang="html">
   <div>
-    <h1>Events</h1>
+    <h1>Create an event</h1>
   </div>
 </template>
-
 <script>
 export default {}
 </script>
-
 <style lang="css" scoped>
 h1 {
-  margin: 0;
+  display: block;
 }
 </style>
